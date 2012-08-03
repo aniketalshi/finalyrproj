b226b5c354b808f802cd0bd054a49ece76ebe8c0
Aniket Alshi <aniketalshi@gmail.com>
2012-08-03 19:34:51 +0530

    commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from scipy.misc import imresize
+from scipy.misc import imsave
+import graphcut
+from numpy import *
+from PIL import Image
+from pylab import *
+
+im = array(Image.open('empire.jpg'))
+im = imresize(im,0.04,interp='bilinear')
+size = im.shape[:2]
+print size
+
+# add two rectangular training regions
+labels = zeros(size)
+labels[3:18,3:18] = -1
+labels[-18:-3,-18:-3] = 1
+
+# create graph
+g = graphcut.build_bayes_graph(im,labels,kappa=1)
+
+
+out=open("tocpp.txt",'w')
+out.write(str(len(g))+'\n')
+
+edlist=g.edges()
+for a in edlist:
+    out.write(str(a[0]) + ' ' + str(a[1]) + ' ' + str(g.edge_weight((a[0],a[1])))+'\n')
+out.write(str(-1)+' '+str(-1)+' '+str(-1)+'\n')
+out.close()
+
+
+
+
+# cut the graph
+res = graphcut.cut_graph(g,size)
+
+
+#figure()
+#graphcut.show_labeling(im,labels)
+figure()
+imshow(res)
+gray()
+axis('off')
+show()
